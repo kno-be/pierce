7893b9e8ec85bc1f56413c9fe1de47d0adec95c2
Bernardo Knoblauch <67606710+kno-be@users.noreply.github.com>
2021-06-12 23:02:12 -0300

    upd: build corrections

--- pages/admin.js
+++ pages/admin.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import React from 'react';
 import { useForm } from "react-hook-form";
 import Footer from '../components/footer/footer';
